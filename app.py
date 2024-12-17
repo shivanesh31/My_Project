168c801572813cf043d594f121030f26dbf98daa
Shivanesh Sivakumar <89888463+shivanesh31@users.noreply.github.com>
2024-12-17 19:46:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-import xgboost as xgb
-import pickle
+pip install requirement.txt
 
 
 def load_data():
